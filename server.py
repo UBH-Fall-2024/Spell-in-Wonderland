74e5ac8e5a6f94c9464a256e005df64a8335d370
Wonwoo Jeong <warrenww14@gmail.com>
2024-11-09 16:11:36 -0500

    server.py starts the app.

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, Blueprint
 from pymongo import MongoClient
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
 
 client = MongoClient("mongo")
 db = client["Spell-in-Wonderland"]
@@ -14,3 +14,6 @@ from spell import spell_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(spell_bp)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
