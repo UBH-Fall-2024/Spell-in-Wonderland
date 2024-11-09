df7507e5ea4558e59e91271ef02bd59706fd2522
jespbel <jmbelaso@buffalo.edu>
2024-11-09 18:02:43 -0500

    Puts an id in the database

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 from flask import Flask, Blueprint
 from pymongo import MongoClient
 import csv
+import random
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -17,8 +18,7 @@ app.register_blueprint(spell_bp)
 def csv_to_db():
     reader = csv.DictReader(open('word.csv'))
     for row in reader:
-        word.insert_one({'difficulty':row[0]})
-        word.insert_one({'word':row[1]})
+        word.insert_one({{'difficulty':row[0]}, {'id':random.randint(0, 200)}, {'word':row[1]}})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
