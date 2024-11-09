221981b13a93ff56895f615066895ad3beb85cb7
Wonwoo Jeong <warrenww14@gmail.com>
2024-11-09 16:10:20 -0500

    Added route for homepage.

--- home.py
+++ home.py
@@ -1,10 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, send_file, make_response
 from pymongo import MongoClient
 
 home_bp = Blueprint('home_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
-@home_bp.route('/baz.bar')
-def foo():
-    pass
+@home_bp.route('/home', methods=["GET"])
+def home():
+    response = send_file('./templates/home.html', mimetype='text/html')
+    return make_response(response)
+
+@home_bp.route('/templates/styles.css', methods=["GET"])
+def serve_home_css():
+    response = send_file('./templates/styles.css', mimetype='text/css')
+    return make_response(response)
