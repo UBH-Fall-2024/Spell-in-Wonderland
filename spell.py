6bcb9eea88a9a4f94667638c53314650b9e21e85
Wonwoo Jeong <warrenww14@gmail.com>
2024-11-09 18:35:39 -0500

    Spell path added.

--- spell.py
+++ spell.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, send_file, make_response
 from pymongo import MongoClient
 import boto3
 
@@ -6,6 +6,12 @@ spell_bp = Blueprint('spell_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
-@spell_bp.route('/baz.bar')
-def foo():
-    pass
+@spell_bp.route('/spell', methods=["GET"])
+def home():
+    response = send_file('./templates/spell.html', mimetype='text/html')
+    return make_response(response)
+
+@spell_bp.route('/spellStyles.css', methods=["GET"])
+def serve_home_css():
+    response = send_file('./templates/spellStyles.css', mimetype='text/css')
+    return make_response(response)
