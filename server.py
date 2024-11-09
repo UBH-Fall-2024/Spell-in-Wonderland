4f4160e03da42806448a9c37f9eb11360f9d78ef
jespbel <jmbelaso@buffalo.edu>
2024-11-09 16:15:20 -0500

    Just added stuff to build the database

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, Blueprint
 from pymongo import MongoClient
+import csv
 
 app = Flask(__name__)
 
@@ -7,10 +8,14 @@ client = MongoClient("mongo")
 db = client["Spell-in-Wonderland"]
 word = db['word']
 
-
-
 from home import home_bp
 from spell import spell_bp
 
 app.register_blueprint(home_bp)
 app.register_blueprint(spell_bp)
+
+def csv_to_db():
+    reader = csv.DictReader(open('word.csv'))
+    for row in reader:
+        word.insert_one({'difficulty':row[0]})
+        word.insert_one({'word':row[1]})
