fd84292a9960884c90b59c0b0b55b3d150b96f76
jespbel <jmbelaso@buffalo.edu>
2024-11-09 19:23:35 -0500

    Can now populate database

--- home.py
+++ home.py
@@ -1,13 +1,31 @@
 from flask import Blueprint, send_file, make_response
 from pymongo import MongoClient
+import csv
+import random
+
+client = MongoClient("mongo")
+db = client["Spell-in-Wonderland"]
+word = db['word']
 
 home_bp = Blueprint('home_bp', __name__,
     template_folder='templates',
     static_folder='static')
 
+def csv_to_db():
+    word.remove({})
+    ids = []
+    with open('words.csv', 'r') as words:
+        reader = csv.DictReader(words, fieldnames=None)
+        for row in reader:
+            print(row)
+            id = random.randint(0, 200)
+            if id not in ids:
+                word.insert_one({'id': id, 'word':row['Word'], 'difficulty':row['Difficulty']})
+
 @home_bp.route('/home', methods=["GET"])
 def home():
     response = send_file('./templates/home.html', mimetype='text/html')
+    csv_to_db()
     return make_response(response)
 
 @home_bp.route('/styles.css', methods=["GET"])
--- server.py
+++ server.py
@@ -15,11 +15,6 @@ from spell import spell_bp
 app.register_blueprint(home_bp)
 app.register_blueprint(spell_bp)
 
-def csv_to_db():
-    reader = csv.DictReader(open('word.csv'))
-    for row in reader:
-        word.insert_one({{'difficulty':row[0],'id':random.randint(0, 200), 'word':row[1]}})
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
 
