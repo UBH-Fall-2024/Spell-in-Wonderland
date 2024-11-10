81574a94047b52668e8a2b84e44c8b78acbee549
Wonwoo Jeong <warrenww14@gmail.com>
2024-11-10 06:05:10 -0500

    Removed secure flag. Might need it when we do https.

--- home.py
+++ home.py
@@ -28,7 +28,7 @@ def csv_to_db():
 def home():
     response = send_file('./templates/home.html', mimetype='text/html')
     if 'user_id' not in request.cookies:
-        response.set_cookie("user_id", value=add_user(secrets.token_urlsafe(80)), httponly=True, secure=True)
+        response.set_cookie("user_id", value=add_user(secrets.token_urlsafe(80)), httponly=True) #secure=True)
     response.headers["X-Content-Type-Options"] = "nosniff"
     csv_to_db()
     return make_response(response)
